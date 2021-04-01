ac482dd4c5a65429b402c3963b4762d4606b15ce
HJCDstudios <78640306+HJCDstudios@users.noreply.github.com>
2021-04-01 22:19:11 +0800

    Update Api.js

--- docs/Api.js
+++ docs/Api.js
@@ -1,7 +1,7 @@
 var githubApi = {};
 githubApi.header = {};
 githubApi.header.Accept = "application/vnd.github.v3+json"; //make sure we use v3
-githubApi.header.Authorization = "token <token>";
+githubApi.header.Authorization = "token REDACTED-TOKEN";
 githubApi.baseUrl = "https://api.github.com";
 githubApi.username = "";
 githubApi.nameRepo = "";
@@ -35,16 +35,25 @@ githubApi.header = {};
 githubApi.header.Accept = "application/vnd.github.v3+json"; //make sure we use v3
 githubApi.header.Authorization = "token <token>";
 githubApi.baseUrl = "https://api.github.com";
-githubApi.username = "";
+githubApi.username = "HJCDstudios";
 githubApi.nameRepo = "";
 githubApi.sha = "";
 githubApi.path = "";
 githubApi.newFile = false;
 }
-function CreateFile(name,repo,filename,path,callback) {
-githubApi.newFile = true;
-githubApi.path = path;
-githubApi.username = name;
-githubApi.nameRepo = repo;
-apiRequest("PUT",githubApi.baseurl,githubApi,callback);
+function CreateFile(name,repo,filename,path,content,msg,callback) {
+var jsonData = new Object();
+	jsonData.message = msg;
+	jsonData.content = btoa(content); //encode64
+	jsonData.path = path;
+	jsonData.branch = "master";
+	if(!githubApi.newFile) {
+		jsonData.sha = githubApi.sha;
+	}
+	var url = githubApi.baseUrl+"/repos/"+githubApi.username+"/"+repo+"/contents/"+path;
+	jsonData = JSON.stringify(jsonData); //api expects json as string
+	cout(jsonData);
+	apiRequest("PUT", url, jsonData, function(r) {
+	  document.write(r);
+	});
 }
