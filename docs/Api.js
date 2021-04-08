1a5135d8ac2d262eb1dd027cf769bd04e297b776
HJCDstudios <78640306+HJCDstudios@users.noreply.github.com>
2021-04-08 09:21:21 +0800

    Update Api.js

--- docs/Api.js
+++ docs/Api.js
@@ -2,21 +2,15 @@ var githubApi = {};
 githubApi.header = {};
 githubApi.header.Accept = "application/vnd.github.v3+json"; //make sure we use v3
 githubApi.header.Authorization = "token REDACTED-TOKEN";
-githubApi.baseUrl = "https://api.github.com";
-githubApi.username = "";
-githubApi.nameRepo = "";
-githubApi.sha = "";
-githubApi.path = "";
-githubApi.newFile = false;
 function apiRequest(method, url, jsonData, callback) {
   //load the json file
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 ){
       if (xhr.status == 200 || xhr.status == 201) {
-        callback(xhr.responseText);
+        callback(false,xhr.responseText);
       } else {
-        callback(xhr.responseText);
+        callback(true,xhr.responseText);
       }
     }
   }
@@ -25,34 +19,18 @@ function apiRequest(method, url, jsonData, callback) {
     xhr.setRequestHeader(key, githubApi.header[key]);
   }
   xhr.send(jsonData);
-  ResetApi();
 }
-function cout(){}
-function ResetApi() {
-githubApi = {};
-githubApi.header = {};
-githubApi.header.Accept = "application/vnd.github.v3+json"; //make sure we use v3
-githubApi.header.Authorization = "token ";
-githubApi.baseUrl = "https://api.github.com";
-githubApi.username = "HJCDstudios";
-githubApi.nameRepo = "";
-githubApi.sha = "";
-githubApi.path = "";
-githubApi.newFile = false;
+function CreateFile(ob) {
+  var jsonData = new Object();
+  jsonData.message = ob.CommitMsg;
+  jsonData.content = btoa(ob.Content); //encode64
+  jsonData.path = ob.Path;
+  jsonData.branch = ob.Branch;
+  var url = "https://api.github.com/repos/" + ob.UserName + "/" + ob.RepoName + "/contents/" + ob.Path;
+  jsonData = JSON.stringify(jsonData); //api expects json as string
+  apiRequest("PUT", url, jsonData,cb);
 }
-function CreateFile(name,repo,filename,path,content,msg,callback) {
-var jsonData = new Object();
-	jsonData.message = msg;
-	jsonData.content = btoa(content); //encode64
-	jsonData.path = path;
-	jsonData.branch = "master";
-	if(!githubApi.newFile) {
-		jsonData.sha = githubApi.sha;
-	}
-	var url = githubApi.baseUrl+"/repos/"+name+"/"+repo+"/contents/"+path;
-	jsonData = JSON.stringify(jsonData); //api expects json as string
-	cout(jsonData);
-	apiRequest("PUT", url, jsonData, function(r) {
-	  document.write(r);
-	});
+function DeleteFile(ob) {
+  var jsonData = new Object();
+  jsonData.
 }
